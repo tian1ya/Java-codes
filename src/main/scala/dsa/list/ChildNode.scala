78642b7f8442919ff58c728cc10354929c20545d
liuxu <xuxliu@thoughtworks.com>
2021-01-23 17:30:38 +0800

    refactor scala file catalog

--- src/main/scala/dsa/list/ChildNode.scala
+++
@@ -1,6 +0,0 @@
-package dsa.list
-
-class ChildNode(val cNo: Int) {
-  val no: Int = cNo
-  var next: ChildNode = null
-}
--- src/main/scala/dsa/list/HeroNode.scala
+++
@@ -1,8 +0,0 @@
-package dsa.list
-
-class HeroNode(hNo: Int, hName: String, hNickName:String) {
-  val no: Int = hNo
-  var name:String = hName
-  var nickName: String = hNickName
-  var next:HeroNode = null;
-}
