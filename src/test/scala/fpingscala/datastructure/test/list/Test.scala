d87a62cb1d95ef93455809d914342e82bbe6d721
liuxu <xuxliu@thoughtworks.com>
2021-01-25 13:52:37 +0800

    scala 小红书第三章 list 部分方法时间

---
+++ src/test/scala/fpingscala/datastructure/test/list/Test.scala
@@ -0,0 +1,22 @@
+package fpingscala.datastructure.test.list
+
+import org.junit.Assert._
+import org.junit._
+
+import fpingscala.datastructures._
+
+class Test {
+
+  val list = List[Int];
+
+  @Before
+  def before()= {
+
+  }
+
+  @Test
+  def testList(): Unit = {
+
+  }
+
+}
