b7f84580eff01d5693739c258a5a9a1232ba6eae
xuxliu <xuxliu@thougthworks.com>
2021-04-16 09:18:14 +0800

    minHeap scala code

--- src/main/scala/dsaPlay/tree/MinHeap.scala
+++ src/main/scala/dsaPlay/tree/MinHeap.scala
@@ -12,19 +12,87 @@ class MinHeap[E <: Comparable[E]] {
 
   }
 
-  private def getFamilyIndex(parentIndex: Int): (Int, Int, Int) = (parentIndex, parentIndex * 2+1, parentIndex * 2 + 2)
+  private def getParentIndex(childIndex: Int): Int = childIndex / 2
 
-
-
-  private def siftDown(index: Int): Unit = {
-    // 最小堆，大元素下沉
-    val (parentIndex, leftChildIndex, rightChildIndex) = getFamilyIndex(index)
-
+  def add(e: E): Unit = {
+    data.append(e)
+    siftUp(data.size - 1)
   }
 
+  private def swap(indexA: Int, indexB: Int): Unit = {
+    val temp = data(indexB)
+    data(indexB) = data(indexA)
+    data(indexA) = temp
+  }
+
+  private def siftUp(lastChildIndex: Int): Unit = {
+    if (lastChildIndex > 0 && data(lastChildIndex).compareTo(data(getParentIndex(lastChildIndex))) < 0) {
+      swap(lastChildIndex, getParentIndex(lastChildIndex))
+      siftUp(getParentIndex(lastChildIndex))
+    }
+  }
+
+  def siftDown(index: Int): Unit = {
+    val leftChildIndex = index * 2 + 1
+    val rightChildIndex = leftChildIndex + 1
+
+    var nextTopIndex = leftChildIndex
+
+    if (rightChildIndex < getSize && data(rightChildIndex).compareTo(data(leftChildIndex)) < 0) {
+      // 右孩子比较小
+      nextTopIndex = rightChildIndex
+    }
+
+    if (nextTopIndex < getSize && data(nextTopIndex).compareTo(data(index)) < 0) {
+      swap(index, nextTopIndex)
+      siftDown(nextTopIndex)
+    }
+  }
+
+  private def removeTop(): Option[E] = {
+    var result: Option[E] = null
+
+    if (data.size > 1) {
+      result = Some(data(0))
+      data(0) = data(data.size - 1)
+
+      data.remove(data.size - 1)
+
+      siftDown(0)
+    } else {
+      result = None
+    }
+    result
+  }
+
+  def getSize: Int = data.size
+
+  def remove(): Option[E] = removeTop()
+
+  override def toString: String = "[ " + data.mkString(",") + " ]"
 }
 
 
 object MinHeap extends App {
+  private val minHeap = new MinHeap[Integer](Seq(1, 2, 3, 4, 5, 6))
+
+  testRemove
+
+  def testAdd: Unit = {
+
+    println(minHeap.toString)
+    minHeap.add(0)
+    println(minHeap.toString)
+
+    minHeap.add(9)
+    println(minHeap.toString)
+  }
+
+  def testRemove: Unit = {
+    testAdd
+    val remove = minHeap.remove
+    println("removed: " + remove.get)
+    println(minHeap.toString)
+  }
 
 }
