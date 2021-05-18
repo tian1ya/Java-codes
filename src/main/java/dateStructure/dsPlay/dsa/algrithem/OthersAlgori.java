9e38837f4456471e197547030bca341584b5154d
xuxliu <xuxliu@qq.com>
2021-05-18 10:13:13 +0800

    scala 在做一遍之前Java 的 longestPalindromeDP 题目

--- src/main/java/dateStructure/dsPlay/dsa/algrithem/OthersAlgori.java
+++ src/main/java/dateStructure/dsPlay/dsa/algrithem/OthersAlgori.java
@@ -33,15 +33,15 @@ public class OthersAlgori {
 //        System.out.println(longestPalindrome("ac"));
 //        System.out.println(longestPalindrome("bb"));
 //        System.out.println(longestPalindrome("bbbb"));
-        System.out.println(longestPalindrome("bbbb"));
+//        System.out.println(longestPalindrome("bbbb"));
 
 //        System.out.println(longestPalindromeDP("bbbb"));
-//        System.out.println(longestPalindromeDP("cbbd"));
-//        System.out.println(longestPalindromeDP("ac"));
-//        System.out.println(longestPalindromeDP("bb"));
-//        System.out.println(longestPalindromeDP("bbbb"));
-//        System.out.println(longestPalindromeDP("abb"));
-//        System.out.println(longestPalindromeDP("aacabdkacaa"));
+        System.out.println(longestPalindromeDP("cbbd"));
+        System.out.println(longestPalindromeDP("ac"));
+        System.out.println(longestPalindromeDP("bb"));
+        System.out.println(longestPalindromeDP("bbbb"));
+        System.out.println(longestPalindromeDP("abb"));
+        System.out.println(longestPalindromeDP("aacabdkacaa"));
 
 //        ListNode listNode1 = new ListNode(2, new ListNode(4, new ListNode(3)));
 //        ListNode listNode2 = new ListNode(5, new ListNode(6, new ListNode(4)));
--- src/main/scala/dsaPlay/LongestPalindrome.scala
+++ src/main/scala/dsaPlay/LongestPalindrome.scala
@@ -3,14 +3,7 @@ package dsaPlay
 object LongestPalindrome extends App {
   def longestPalindrome(s: String): String = {
 
-    if (s.length == 1)
-      s
-    else if (s.length == 2) {
-      if (s.charAt(0) == s.charAt(1))
-        s
-      else
-        s.charAt(0).toString
-    } else {
+    if (s.length == 1) s else {
       var result: String = "";
 
       for (i <- 1 until s.length - 1) {
@@ -35,27 +28,73 @@ object LongestPalindrome extends App {
     }
   }
 
-
   def longestPalindromeInner(s: String, prevIndex: Int, nextIndex: Int, result: String): String = {
-    if (prevIndex < 0 || nextIndex >= s.length || prevIndex > nextIndex)
-      result
+    if (prevIndex < 0 || nextIndex >= s.length || prevIndex > nextIndex) result
     else {
       val prevStr = s.charAt(prevIndex).toString
       val nextSte = s.charAt(nextIndex).toString
 
-      if (prevStr == nextSte) {
-        val res: String = prevStr + result + prevStr
-        longestPalindromeInner(s, prevIndex - 1, nextIndex + 1, res)
-      } else result
+      if (prevStr == nextSte)
+        longestPalindromeInner(s, prevIndex - 1, nextIndex + 1, prevStr + result + prevStr)
+      else result
+    }
+  }
+
+  def longestPalindromeDP(s: String): String = {
+    if (s.length == 1) s.charAt(0).toString
+    else {
+      var result: String = ""
+
+      val length = s.length
+      val dp: Array[Array[Int]] = Array.ofDim[Int](length, length)
+
+      /*
+          初始条件： dp[i][i] = 1 表示已经是回文字
+          dp[i][i] = 0 where i != j
+       */
+      for (index <- 0 until length)
+        dp(index)(index) = 1
+
+      /*
+         状态转移方程
+         dp[i][j] = dp[i][i] && dp[i+1][j-1]
+       */
+      for (endIndex <- 1 until length) {
+        for (startIndex <- 0 until endIndex) {
+          if (s.charAt(startIndex) == s.charAt(endIndex)) {
+            if (endIndex - startIndex < 3) {
+              /*
+                会有这样的情况 abb， 这个时候 endIndex = 3， startIndex = 2
+                如果使用逻辑 dp(startIndex + 1)(endIndex - 1)， 那么就会得逻辑得到所有差乱
+                startIndex=2+1=3
+                endIndex=3-1=2
+               */
+              dp(startIndex)(endIndex) = 1
+            } else
+              dp(startIndex)(endIndex) = dp(startIndex + 1)(endIndex - 1)
+
+            if (dp(startIndex)(endIndex) == 1 && s.substring(startIndex, endIndex + 1).length > result.length)
+              result = s.substring(startIndex, endIndex + 1)
+          }
+        }
+      }
+      if (result.nonEmpty) result else s.charAt(0).toString
     }
   }
 
   //  println(longestPalindrome("babad"))
   //  println(longestPalindrome("b"))
   //  println(longestPalindrome("bbb"))
-  println(longestPalindrome("bbbb"))
+  //  println(longestPalindrome("bbbb"))
   //  println(longestPalindrome("cbbd"))
   //  println(longestPalindrome("abb"))
   //  println(longestPalindrome("aacabdkacaa"))
+
+  println(longestPalindromeDP("bbbb"))
+  println(longestPalindromeDP("aacabdkacaa"))
+  println(longestPalindromeDP("cbbd"))
+  println(longestPalindromeDP("babad"))
+  println(longestPalindromeDP("dsfradc"))
+
 
 }
