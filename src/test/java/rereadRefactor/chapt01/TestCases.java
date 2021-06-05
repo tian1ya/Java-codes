5657de8148c687c63420d77b6bdca3b87909584d
xuxliu <xuxliu@qq.com>
2021-06-05 21:43:51 +0800

    【重读重构】第一章：重构案例-添加测试

---
+++ src/test/java/rereadRefactor/chapt01/TestCases.java
@@ -0,0 +1,24 @@
+package rereadRefactor.chapt01;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+public class TestCases {
+    @Test
+    public void test_statement_logic() {
+        Movie movie1 = new Movie("西游记", 2);
+        Rental rental = new Rental(movie1, 2);
+        Customer customer = new Customer("小明");
+        customer.addRental(rental);
+
+        String statement = customer.statement();
+        assertTrue(statement.contains("Rental Records for 小明"));
+        assertTrue(statement.contains("西游记\t1.5"));
+        assertTrue(statement.contains("西游记\t1.5"));
+        assertTrue(statement.contains("Amount owed is 1.5"));
+        assertTrue(statement.contains("You earned 1frequent renter points"));
+        System.out.println(statement);
+    }
+}
