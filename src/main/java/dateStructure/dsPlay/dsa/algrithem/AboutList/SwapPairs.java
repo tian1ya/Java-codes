15c0bb11dddcbe3a320dac47233fe6a3cd737329
“lx” <lx@qq.com>
2022-08-27 15:38:23 +0800

    leetcode swapPairs

--- src/main/java/dateStructure/dsPlay/dsa/algrithem/AboutList/SwapPairs.java
+++ src/main/java/dateStructure/dsPlay/dsa/algrithem/AboutList/SwapPairs.java
@@ -1,5 +1,7 @@
 package dateStructure.dsPlay.dsa.algrithem.AboutList;
 
+import java.util.Objects;
+
 public class SwapPairs {
     public static ListNode swapPairs(ListNode head) {
         ListNode dummyHead = new ListNode();
@@ -21,19 +23,52 @@ public class SwapPairs {
         return dummyHead.next;
     }
 
+    public static ListNode swapPairsV2(ListNode head) {
+        if (Objects.isNull(head) || Objects.isNull(head.next)) return head;
+        ListNode dummyHead = new ListNode();
+        ListNode returnHead = dummyHead;
+
+        while (Objects.nonNull(head)) {
+
+            if (Objects.isNull(head.next)) {
+                // 奇数长度链表
+                dummyHead.next = head;
+                return returnHead.next;
+            }
+
+            // 暂存节点
+            ListNode currentNode = head;
+            ListNode nextNode = head.next;
+            ListNode tailNode = head.next.next;
+
+            // 断开链接
+            currentNode.next = null;
+            nextNode.next = null;
+
+            // 链接新链
+            dummyHead.next = nextNode;
+            nextNode.next = currentNode;
+            dummyHead = currentNode;
+
+            // 原链，往后走2个节点
+            head = tailNode;
+        }
+        return returnHead.next;
+    }
+
 
     public static void main(String[] args) {
         ListNode node1 = new ListNode(1);
-        ListNode node11 = new ListNode(4);
-        ListNode node2 = new ListNode(5);
-        ListNode node22 = new ListNode(6);
+        ListNode node11 = new ListNode(2);
+        ListNode node2 = new ListNode(3);
+//        ListNode node22 = new ListNode(4);
 //        ListNode node3 = new ListNode(7);
         node1.next = node11;
         node11.next = node2;
-        node2.next = node22;
+//        node2.next = node22;
 //        node22.next = node3;
 
         System.out.println(node1);
-        System.out.println(swapPairs(node1));
+        System.out.println(swapPairsV2(node1));
     }
 }
