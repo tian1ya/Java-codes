7b15cc756a9981261afbd3e9646602c7f210f95a
“lx” <lx@qq.com>
2022-09-03 13:31:49 +0800

    leetcode deleteDuplicates 纯循环 暴力

--- src/main/java/dateStructure/dsPlay/dsa/algrithem/AboutList/DeleteDuplicates.java
+++ src/main/java/dateStructure/dsPlay/dsa/algrithem/AboutList/DeleteDuplicates.java
@@ -1,36 +1,38 @@
 package dateStructure.dsPlay.dsa.algrithem.AboutList;
 
+import java.util.Objects;
+
 public class DeleteDuplicates {
     // 83
     public static ListNode deleteDuplicates(ListNode head) {
         if (head == null || head.next == null) return head;
 
-        head.next = deleteDuplicates(head.next);
-        return head.val == head.next.val ? head.next : head;
-    }
+        ListNode dummyHead = new ListNode(0, head);
+        ListNode cur = dummyHead;
 
-    public static ListNode deleteDuplicatesV2(ListNode head) {
-        if (head == null || head.next == null) return head;
-
-        ListNode dummyHead = head;
-        while (dummyHead != null && dummyHead.next != null) {
-            if (dummyHead.val == dummyHead.next.val) {
-                dummyHead.next = dummyHead.next.next;
-            } else
-                dummyHead = dummyHead.next;
+        while (Objects.nonNull(cur.next) && Objects.nonNull(cur.next.next)) {
+            int val = cur.next.val;
+            if (val == cur.next.next.val) {
+                while (Objects.nonNull(cur.next) && cur.next.val == val) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
         }
 
-        return head;
+        return dummyHead.next;
     }
+
 
     public static void main(String[] args) {
         ListNode node1 = new ListNode(1);
-        ListNode node11 = new ListNode(1);
-        ListNode node2 = new ListNode(2);
-        ListNode node22 = new ListNode(2);
+        ListNode node11 = new ListNode(2);
+        ListNode node2 = new ListNode(3);
+        ListNode node22 = new ListNode(3);
         ListNode node3 = new ListNode(4);
-        ListNode node4 = new ListNode(4);
-        ListNode node5 = new ListNode(4);
+        ListNode node4 = new ListNode(5);
+        ListNode node5 = new ListNode(6);
         node1.next = node11;
         node11.next = node2;
         node2.next = node22;
@@ -39,6 +41,7 @@ public class DeleteDuplicates {
         node4.next = node5;
 
 //        System.out.println(deleteDuplicates(node1));
-        System.out.println(deleteDuplicatesV2(node1));
+        System.out.println(node1);
+        System.out.println(deleteDuplicates(node1));
     }
 }
