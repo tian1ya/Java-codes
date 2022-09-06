c16264cac063ac10198b0a4ca0af3416e9a1cadc
“lx” <lx@qq.com>
2022-09-06 23:09:39 +0800

    leetcode 链表 reverseBetween 减少循环

--- src/main/java/dateStructure/dsPlay/dsa/algrithem/AboutList/ReverseBetween.java
+++ src/main/java/dateStructure/dsPlay/dsa/algrithem/AboutList/ReverseBetween.java
@@ -16,11 +16,11 @@ public class ReverseBetween {
         ListNode endNode = new ListNode();
         while (index < right) {
 
-            if (left-1 == index) {
+            if (left - 1 == index) {
                 preNode = dummyHead;
             }
 
-            if (right-1 == index) {
+            if (right - 1 == index) {
                 endNode = dummyHead.next;
             }
             dummyHead = dummyHead.next;
@@ -54,6 +54,24 @@ public class ReverseBetween {
         return prev;
     }
 
+    public static ListNode reverseBetweenV2(ListNode head, int left, int right) {
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode cur = pre.next;
+        ListNode next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur.next;
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return dummyNode.next;
+    }
+
     public static void main(String[] args) {
 
         ListNode node1 = new ListNode(1);
@@ -64,14 +82,14 @@ public class ReverseBetween {
 //        ListNode node4 = new ListNode(6);
 //        ListNode node5 = new ListNode(6);
         node1.next = node11;
-//        node11.next = node2;
-//        node2.next = node22;
-//        node22.next = node3;
+        node11.next = node2;
+        node2.next = node22;
+        node22.next = node3;
 //        node3.next = node4;
 //        node4.next = node5;
 
         System.out.println(node1);
-        System.out.println(reverseBetween(node1, 1, 2));
+        System.out.println(reverseBetweenV2(node1, 2, 4));
 //        System.out.println(reverseBetween(node1, 1, 2));
 //        System.out.println(reverseList(node1, 1));
     }
